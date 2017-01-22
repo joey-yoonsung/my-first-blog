906c1b2187e6b4655c82fc672e782219afddb200
sw.yoonsung <sw.yoonsung@gmail.com>
2017-01-22 12:06:36 +0900

    My Django Girls app, 첫번째 커밋

---
+++ myenv/IntroProject/python_intor.py
@@ -0,0 +1,25 @@
+print("Hello, Django girls!")
+
+if 3>2:
+	print('It Works')
+
+
+	def hi():
+		print('Hi, there!')
+		print('How are you?')
+
+	def hi(name):
+		print('Hi '+ name)
+
+
+# hi()
+
+girls=['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+for name in girls:
+	hi(name)
+	print('next girl\n')
+
+
+for i in range(1, 6):
+	print(i)
