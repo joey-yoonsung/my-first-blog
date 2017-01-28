a71f6d55fc2835c34501fd8a5b68a331487c638e
yoonsung-xnote <sw.yoonsung@gmail.com>
2017-01-28 21:25:17 +0900

    Changed the HTML for the site.

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/yoonsung-xnote/PythonProjects/djangogirls/my-first-blog/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
